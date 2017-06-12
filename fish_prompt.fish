34cf62fa98627502d5991e2730513936f846c758
Tristan Pemble <tpemble@gmail.com>
2017-06-12 01:09:20 -0700

    Add fish prompt;

---
+++ fish_prompt.fish
@@ -0,0 +1,57 @@
+# You can override some default options with config.fish:
+#
+#  set -g theme_short_path yes
+
+function fish_prompt
+  set -l last_command_status $status
+  set -l cwd
+
+  if test "$theme_short_path" = 'yes'
+    set cwd (basename (prompt_pwd))
+  else
+    set cwd (prompt_pwd)
+  end
+
+  set -l lambda   "λ"
+  set -l ahead    "↑"
+  set -l behind   "↓"
+  set -l diverged "⥄ "
+  set -l dirty    "⨯"
+  set -l none     "◦"
+
+  set -l normal_color     (set_color normal)
+  set -l success_color    (set_color white --bold)
+  set -l error_color      (set_color red --bold)
+  set -l directory_color  (set_color blue)
+  set -l repository_color (set_color green)
+
+  if git_is_repo
+    if test "$theme_short_path" = 'yes'
+      set root_folder (command git rev-parse --show-toplevel ^/dev/null)
+      set parent_root_folder (dirname $root_folder)
+      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+
+      echo -n -s $directory_color $cwd $normal_color " "
+    else
+      echo -n -s $directory_color $cwd $normal_color " "
+    end
+
+    if git_is_touched
+      echo -n -s $dirty
+    else
+      echo -n -s (git_ahead $ahead $behind $diverged $none)
+    end
+
+    echo -n -s " " $repository_color (git_branch_name) $normal_color " "
+  else
+    echo -n -s $directory_color $cwd $normal_color " "
+  end
+
+  if test $last_command_status -eq 0
+    echo -n -s $success_color $lambda $normal_color
+  else
+    echo -n -s $error_color $lambda $normal_color
+  end
+
+  echo -n -s " "
+end
